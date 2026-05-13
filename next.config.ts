0d11bdbd68d211f65f4055c0eb9ddac33c82a73b
aurorafoodie <aurora.dimaio99@gmail.com>
2026-05-13 21:55:26 +0100

    refactor: update image configuration to use remotePatterns instead of domains

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: [
-      "render.worldofwarcraft.com",
-      "blzmedia-a.akamaihd.net",
-      "eu.api.blizzard.com",
-    ],
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "eu.api.blizzard.com",
-        pathname: "/data/wow/media/**",
-      },
+      { protocol: "https", hostname: "render.worldofwarcraft.com" },
+      { protocol: "https", hostname: "blzmedia-a.akamaihd.net" },
+      { protocol: "https", hostname: "eu.api.blizzard.com" },
     ],
   },
 };
